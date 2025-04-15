a952e21e68706c2ea03272400a8780fb6b7bfcad
David Galdamez <dgaldamez77@gmail.com>
2025-04-15 16:57:35 -0500

    Initial for dashboard

--- scripts/runjob.sh
+++ scripts/runjob.sh
@@ -1,6 +1,1 @@
-export DB_NAME=cosmocargo
-export DB_USER=cosmocargo_service
-export DB_PASSWORD=cosmocargo
-export DB_HOST=localhost
-export DB_PORT=5432
-python3 ../src/etl/etl.py ../data/sample_data.json >> ../logs/etl.log 2>&1 
+DB_NAME=cosmocargo DB_USER=cosmocargo_service DB_PASSWORD=cosmocargo DB_HOST=localhost DB_PORT=5432 python3 /Users/david/code/python/cosmocargo/src/etl/etl.py /Users/david/code/python/cosmocargo/data/sample_data.json >> /Users/david/code/python/cosmocargo/logs/etl.log 2>&1 
